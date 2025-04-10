a66f994959a36bbdf8527b4d2806d65743c3aee7
LuigiRac <lui.rac06@gmail.com>
2025-04-10 11:18:22 +0200

    creato nuovo array

--- server.php
+++ server.php
@@ -4,3 +4,12 @@ $album_json = file_get_contents('./albums.json');
 $album = json_decode($album_json, true);
 
 // var_dump($album);
+
+
+$album[] = [
+    'titolo' => $titolo,
+    'artista' => $artista,
+    'url_cover' => $url_cover,
+    'anno' => $anno,
+    'genere' => $genere
+];
